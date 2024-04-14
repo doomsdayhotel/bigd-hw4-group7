825d296471e1a3e8d60da36fe93a66c504be7ff9
doomsdayhotel <qyang66@fordham.edu>
2024-04-14 07:21:47 -0400

    Update csv_big_spender.py

--- part_2_storage/queries/csv_big_spender.py
+++ part_2_storage/queries/csv_big_spender.py
@@ -9,12 +9,14 @@ Usage:
 # Import command line arguments and helper functions
 import sys
 import bench
+import numpy as np
+import pandas as pd
 
 # And pyspark.sql to get the spark session
 from pyspark.sql import SparkSession
 
 
-def csv_big_spender(spark, file_path):
+def csv_big_spender(spark, datasets):
     '''Construct a basic query on the people dataset
 
     This function returns a uncomputed dataframe that
@@ -50,20 +52,50 @@ def main(spark, file_path):
     spark : SparkSession object
     which_dataset : string, size of dataset to be analyzed
     '''
-    times = bench.benchmark(spark, 25, csv_big_spender, file_path)
+    # times = bench.benchmark(spark, 25, csv_big_spender, file_path)
 
-    min_time = min(times)
-    max_time = max(times)
-    median_time = np.median(times)
+    # min_time = min(times)
+    # max_time = max(times)
+    # median_time = np.median(times)
 
-    print(f'Times to run csv_big_spender 25 times on {file_path}:')
-    print(times)
-    print(f'Minimum Time: {min_time}')
-    print(f'Maximum Time: {max_time}')
-    print(f'Median Time: {median_time}')
+    # print(f'Times to run csv_big_spender 25 times on {file_path}:')
+    # print(times)
+    # print(f'Minimum Time: {min_time}')
+    # print(f'Maximum Time: {max_time}')
+    # print(f'Median Time: {median_time}')
     
-    df = csv_big_spender(spark, file_path)
-    df.show()
+    # #to make sure the query ran successfully
+    # df = csv_big_spender(spark, file_path)
+    # df.show()
+
+    timing_results = {}
+
+    # Loop over the datasets and collect timing information
+    for file_path in datasets:
+        df = csv_big_spender(spark, file_path)
+        
+        print(f"Results for dataset {file_path}:")
+        df.show()
+
+
+        times = bench.benchmark(spark, 25, csv_big_spender, file_path)
+
+        timing_results[file_path] = {
+            'min_time': min(times),
+            'max_time': max(times),
+            'median_time': np.median(times)
+        }
+        # If you want to see the results immediately after computation
+        print(f'Times to run csv_sum_orders 25 times on {file_path}:')
+        print(timing_results[file_path])
+
+    pd_df = pd.DataFrame(timing_results)
+
+    pd_df.reset_index(inplace=True)
+    pd_df.rename(columns={'index': 'Dataset'}, inplace=True)       
+
+    spark_df = spark.createDataFrame(pd_df)
+    spark_df.show() 
 
 # Only enter this block if we're in main
 if __name__ == "__main__":
@@ -72,6 +104,16 @@ if __name__ == "__main__":
     spark = SparkSession.builder.appName('part2').getOrCreate()
 
     # Get file_path for dataset to analyze
-    file_path = sys.argv[1]
+    # file_path = sys.argv[1]
 
-    main(spark, file_path)
+    # main(spark, file_path)
+
+    # List of datasets to process
+    datasets = [
+        'hdfs:/user/pw44_nyu_edu/peopleSmall.csv',
+        'hdfs:/user/pw44_nyu_edu/peopleModerate.csv',
+        # 'hdfs:/user/pw44_nyu_edu/peopleBig.csv'
+    ]
+    
+    # Call main function with the list of datasets
+    main(spark, datasets)
