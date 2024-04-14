03b48379c279591b3d187ec198cd210e639eea45
doomsdayhotel <qyang66@fordham.edu>
2024-04-14 19:48:28 -0400

    Update convert_csv_to_parquet.py

--- part_2_storage/convert_csv_to_parquet.py
+++ part_2_storage/convert_csv_to_parquet.py
@@ -26,14 +26,24 @@ def main(spark):
     #Use this template to as much as you want for your parquet saving and optimizations!
     # Read the CSV files
     # or instead of inferSchema, schema='first_name STRING, last_name STRING, age INT, income FLOAT, zipcode INT, orders INT, loyalty BOOLEAN, rewards BOOLEAN'? I forgot which one I used, but it worked
-    df_small = spark.read.csv('hdfs:/user/pw44_nyu_edu/peopleSmall.csv', header=True, inferSchema=True)
-    df_moderate = spark.read.csv('hdfs:/user/pw44_nyu_edu/peopleModerate.csv', header=True, inferSchema=True)
-    df_big = spark.read.csv('hdfs:/user/pw44_nyu_edu/peopleBig.csv', header=True, inferSchema=True)
+    
+    # df_small = spark.read.csv('hdfs:/user/pw44_nyu_edu/peopleSmall.csv', header=True, inferSchema=True)
+    # df_moderate = spark.read.csv('hdfs:/user/pw44_nyu_edu/peopleModerate.csv', header=True, inferSchema=True)
+    # df_big = spark.read.csv('hdfs:/user/pw44_nyu_edu/peopleBig.csv', header=True, inferSchema=True)
 
-    # Convert to Parquet and save to HDFS directory
-    df_small.write.parquet('hdfs:/user/qy561_nyu_edu/peopleSmall.parquet')
-    df_moderate.write.parquet('hdfs:/user/qy561_nyu_edu/peopleModerate.parquet')
-    df_big.write.parquet('hdfs:/user/qy561_nyu_edu/peopleBig.parquet')
+    # # Convert to Parquet and save to HDFS directory
+    # df_small.write.parquet('hdfs:/user/qy561_nyu_edu/peopleSmall.parquet')
+    # df_moderate.write.parquet('hdfs:/user/qy561_nyu_edu/peopleModerate.parquet')
+    # df_big.write.parquet('hdfs:/user/qy561_nyu_edu/peopleBig.parquet')
+
+# Read Parquet file into DataFrame
+    df = spark.read.parquet('hdfs:/user/qy561_nyu_edu/peopleSmall.parquet')  # Replace with your file's path
+
+    # Show contents of DataFrame
+    df.show()
+
+    # Print schema of DataFrame
+    df.printSchema()
 
 # Only enter this block if we're in main
 if __name__ == "__main__":
