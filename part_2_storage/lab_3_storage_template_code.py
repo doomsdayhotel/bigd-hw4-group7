84107636d2337f483dd42e6de02c4af6ff72b409
doomsdayhotel <qyang66@fordham.edu>
2024-04-14 09:08:33 -0400

    Update lab_3_storage_template_code.py

--- part_2_storage/lab_3_storage_template_code.py
+++ part_2_storage/lab_3_storage_template_code.py
@@ -23,15 +23,27 @@ def main(spark):
     #####--------------YOUR CODE STARTS HERE--------------#####
 
     #Use this template to as much as you want for your parquet saving and optimizations!
-    # Read the CSV files
-    df_small = spark.read.csv('hdfs:/user/pw44_nyu_edu/peopleSmall.csv', header=True, schema='first_name STRING, last_name STRING, age INT, income FLOAT, zipcode INT, orders INT, loyalty BOOLEAN, rewards BOOLEAN')
-    df_moderate = spark.read.csv('hdfs:/user/pw44_nyu_edu/peopleModerate.csv', header=True, schema='first_name STRING, last_name STRING, age INT, income FLOAT, zipcode INT, orders INT, loyalty BOOLEAN, rewards BOOLEAN')
-    df_big = spark.read.csv('hdfs:/user/pw44_nyu_edu/peopleBig.csv', header=True, schema='first_name STRING, last_name STRING, age INT, income FLOAT, zipcode INT, orders INT, loyalty BOOLEAN, rewards BOOLEAN')
+    # # Read the CSV files
+    # df_small = spark.read.csv('hdfs:/user/pw44_nyu_edu/peopleSmall.csv', header=True, schema='first_name STRING, last_name STRING, age INT, income FLOAT, zipcode INT, orders INT, loyalty BOOLEAN, rewards BOOLEAN')
+    # df_moderate = spark.read.csv('hdfs:/user/pw44_nyu_edu/peopleModerate.csv', header=True, schema='first_name STRING, last_name STRING, age INT, income FLOAT, zipcode INT, orders INT, loyalty BOOLEAN, rewards BOOLEAN')
+    # df_big = spark.read.csv('hdfs:/user/pw44_nyu_edu/peopleBig.csv', header=True, schema='first_name STRING, last_name STRING, age INT, income FLOAT, zipcode INT, orders INT, loyalty BOOLEAN, rewards BOOLEAN')
 
-    # Convert to Parquet and save to HDFS directory
-    df_small.write.mode('overwrite').parquet('hdfs:/user/qy561_nyu_edu/peopleSmall.parquet')
-    df_moderate.write.mode('overwrite').parquet('hdfs:/user/qy561_nyu_edu/peopleModerate.parquet')
-    df_big.write.mode('overwrite').parquet('hdfs:/user/qy561_nyu_edu/peopleBig.parquet')
+    # # Convert to Parquet and save to HDFS directory
+    # df_small.write.mode('overwrite').parquet('hdfs:/user/qy561_nyu_edu/peopleSmall.parquet')
+    # df_moderate.write.mode('overwrite').parquet('hdfs:/user/qy561_nyu_edu/peopleModerate.parquet')
+    # df_big.write.mode('overwrite').parquet('hdfs:/user/qy561_nyu_edu/peopleBig.parquet')
+
+    # Replace the path with the actual path to your Parquet file
+    parquet_file_path = 'hdfs:/user/qy561_nyu_edu/peopleSmall.parquet'
+
+    # Read the Parquet file
+    df = spark.read.parquet(parquet_file_path)
+
+    # Show the first few rows of the DataFrame
+    df.show()
+
+    # Print the schema of the DataFrame
+    df.printSchema()
 
 
 # Only enter this block if we're in main
