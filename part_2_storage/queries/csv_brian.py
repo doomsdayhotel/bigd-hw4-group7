8855d4acf7c2af23339c69a17bb96c80934a0f56
Iris Lu <b05409028@ntu.edu>
2024-04-15 17:46:31 -0400

    update main structure

--- part_2_storage/queries/csv_brian.py
+++ part_2_storage/queries/csv_brian.py
@@ -13,6 +13,7 @@ import bench
 # And pyspark.sql to get the spark session
 from pyspark.sql import SparkSession
 import numpy as np
+import pandas as pd
 
 
 def csv_brian(spark, file_path):
@@ -59,14 +60,44 @@ def main(spark, datasets):
     spark : SparkSession object
     which_dataset : string, size of dataset to be analyzed
     '''
+    # for file_path in datasets:
+    #     times = bench.benchmark(spark, 25, csv_brian, file_path)
+
+    #     print(f'Times to run \'csv_brian\' Query 25 times on {file_path}')
+    #     # print(times)
+    #     print(f'Maximum Time :{max(times)}')
+    #     print(f'minimum Time :{min(times)}')
+    #     print(f'median Time :{np.median(times)}')
+
+    timing_results = {}
+
+    # Loop over the datasets and collect timing information
     for file_path in datasets:
+        #to make sure the query successfully ran. **already checked.** 
+        # df = csv_brian(spark, file_path)
+        
+        # print(f"Results for dataset {file_path}:")
+        # df.show()
+
+
         times = bench.benchmark(spark, 25, csv_brian, file_path)
 
-        print(f'Times to run \'csv_brian\' Query 25 times on {file_path}')
-        # print(times)
-        print(f'Maximum Time :{max(times)}')
-        print(f'minimum Time :{min(times)}')
-        print(f'median Time :{np.median(times)}')
+        timing_results[file_path] = {
+            'min_time': min(times),
+            'max_time': max(times),
+            'median_time': np.median(times)
+        }
+        # If you want to see the results immediately after computation
+        print(f'Times to run csv_brian 25 times on {file_path}:')
+        print(timing_results[file_path])
+
+    pd_df = pd.DataFrame(timing_results)
+
+    pd_df.reset_index(inplace=True)
+    pd_df.rename(columns={'index': 'Dataset'}, inplace=True)       
+
+    spark_df = spark.createDataFrame(pd_df)
+    spark_df.show() 
 
 # Only enter this block if we're in main
 if __name__ == "__main__":
