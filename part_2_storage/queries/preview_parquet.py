6fc951422cadf9f6b3fcfa34fa808c39f0b455b0
doomsdayhotel <qyang66@fordham.edu>
2024-04-16 00:31:42 -0400

    Update preview_parquet.py

--- part_2_storage/queries/preview_parquet.py
+++ part_2_storage/queries/preview_parquet.py
@@ -26,7 +26,7 @@ def main(spark):
     #Use this template to as much as you want for your parquet saving and optimizations!
 
     # Read Parquet file into DataFrame
-    df = spark.read.parquet('hdfs:/user/qy561_nyu_edu/peopleSmallOpt3-2SumOrders.parquet')
+    df = spark.read.parquet('hdfs:/user/qy561_nyu_edu/peopleModerateOpt3-2SumOrders.parquet')
 
     # Show contents of DataFrame
     df.show()
